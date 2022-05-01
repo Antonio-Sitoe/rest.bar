c64f61bb7b285c6a87e1a9e69fd04aa4a6a1be11
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2022-05-01 11:58:20 +0200

    atualizacao

--- README.md
+++ README.md
@@ -1,14 +1,3 @@
 # rest.bar
  frontend do site rest.bar
 
-
- [x] header
- [x] footer 
- [x] main
-
- [x] fazer a busca de dados
-
-[] montar a interface
-      [] verificar se o erro e string  tratar como se fosse string
-      -   [] tratar os erros 
-      [] se for array [] montar a interface
--- scripts/modules/components/ComponentError.js
+++ scripts/modules/components/ComponentError.js
@@ -1,7 +1,12 @@
 import { mainTitle } from '../Context.js';
 
-
 function ComponentError() {
+  const mainListItem = document.querySelectorAll('.main-list li');
+  mainListItem.forEach((li) => {
+    li.addEventListener('click', function (e) {
+      e.preventDefault();
+    });
+  });
   if (mainTitle) {
     mainTitle.innerText = 'Do momento, temos 1 prato disponivel';
   }
